06d5c5855889c62b1a95d9d38f74c6589cdf1bda
Rackspace User <rack@slade-ubuntu-test.(none)>
2013-01-10 10:38:22 -0600

    Should now accept POST and return the JSON

--- flask-gitrcv/gitrcv.py
+++ flask-gitrcv/gitrcv.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request, json
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/rcv', methods=['POST'])
+def rcv_hook():
+    if request.headers['Content-Type'] == 'application/json':
+        print json.dumps(request.json)
+        return json.dumps(request.json)
+    else:
+        return request.headers['Content-Type']
+
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0')
