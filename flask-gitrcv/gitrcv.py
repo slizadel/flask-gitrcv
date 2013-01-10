63e7a414898a2122d2f61f9be1bd6a3a55dfc606
Slade Cozart <slade.cozart@gmail.com>
2013-01-10 16:40:42 -0600

    Debugger information.

--- flask-gitrcv/gitrcv.py
+++ flask-gitrcv/gitrcv.py
@@ -7,12 +7,13 @@ def hello_world():
 
 @app.route('/rcv', methods=['POST'])
 def rcv_hook():
-    app.logger.debug(request.json)
-    if request.headers['Content-Type'] == 'application/json':
-        print json.dumps(request.json)
-        return json.dumps(request.json)
-    else:
-        return request.headers['Content-Type']
+    request_json = request.form.get('payload')
+    app.logger.debug(request_json)
+    app.logger.debug(type(request_json))
+    request_json = json.loads(request_json)
+    app.logger.debug(request_json['repository']['name'])
+
+    return 'Payload'
 
 if __name__ == '__main__':
     app.debug = True
